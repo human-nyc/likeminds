a8f3e3b235851b610413f6a6ab563cfe12c259d0
Rinaldy Laguerre <26579000+rinaldylaguerre@users.noreply.github.com>
2020-01-08 17:45:07 -0500

    footer email link fix

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -4,7 +4,7 @@ import Logo from './Logo'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p><Logo classes='no-text' /> 2020 is made possible by the following people: Rachael Yaeger, Zach Pollakoff, Rahily Sutton, Veronica Hoglund, Joel Fear, Breanna Urquhart, Anya Shcherbakova, Michael Yarinsky, Meredith Jenks, Refersion, ReCharge, Bolt, Care/of vitamins. Want to get involved? <a href='mailto:info@likeminds.com'>Email us!</a></p>
+      <p><Logo classes='no-text' /> 2020 is made possible by the following people: Rachael Yaeger, Zach Pollakoff, Rahily Sutton, Veronica Hoglund, Joel Fear, Breanna Urquhart, Anya Shcherbakova, Michael Yarinsky, Meredith Jenks, Refersion, ReCharge, Bolt, Care/of vitamins. Want to get involved? <a href='mailto:info@likeminds.camp'>Email us!</a></p>
     </div>
   )
 }
