3857c71cd7675cd0eab9dd6d0b3f2d664843033a
sashakluchnik <me@sashakluchnik.com>
2020-03-10 15:46:04 -0400

    footer copy edits

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -4,7 +4,7 @@ import Logo from './Logo'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p><Logo classes='no-text' /> 2020 is made possible by the following people: Rachael Yaeger, Zach Pollakoff, Rahily Sutton, Veronica Hoglund, Joel Fear, Breanna Urquhart, Anya Shcherbakova, Michael Yarinsky, Meredith Jenks, Refersion, ReCharge, Bolt, Care/of vitamins. Want to get involved? <a href='mailto:info@likeminds.camp'>Email us!</a></p>
+      <p><Logo classes='no-text' /> 2020 is made possible by the following people: Rachael Yaeger, Zach Pollakoff, Rahily Sutton, Veronica Hoglund, Joel Fear, Breanna Urquhart, Charlotte Morse, Anya Shcherbakova, Meredith Jenks, Human NYC. Want to get involved? Email us at partnerships@Likeminds.camp.</p>
     </div>
   )
 }
